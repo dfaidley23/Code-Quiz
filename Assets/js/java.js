a4d7c3e166448bd66e62924e4a32dcc5299c20e8
David faidley <david.faidley23@gmail.com>
2022-01-15 19:23:33 -0700

    made changes to All files and added a new JS script

--- Assets/js/java.js
+++ Assets/js/java.js
@@ -12,9 +12,6 @@ var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
 function startQuiz() {
   // hide start screen
@@ -74,14 +71,8 @@ function questionClick() {
     // display new time on page
     timerEl.textContent = time;
 
-    // play "wrong" sound effect
-    sfxWrong.play();
-
     feedbackEl.textContent = "Wrong!";
   } else {
-    // play "right" sound effect
-    sfxRight.play();
-
     feedbackEl.textContent = "Correct!";
   }
 
@@ -150,7 +141,7 @@ function saveHighscore() {
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
     // redirect to next page
-    window.location.href = "highscores.html";
+    window.location.href = "highscorespage.html";
   }
 }
 
@@ -168,35 +159,3 @@ submitBtn.onclick = saveHighscore;
 startBtn.onclick = startQuiz;
 
 initialsEl.onkeyup = checkForEnter;
-
-
-// score functionality
-function printHighscores() {
-    // either get scores from localstorage or set to empty array
-    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-  
-    // sort highscores by score property in descending order
-    highscores.sort(function(a, b) {
-      return b.score - a.score;
-    });
-  
-    highscores.forEach(function(score) {
-      // create li tag for each high score
-      var liTag = document.createElement("li");
-      liTag.textContent = score.initials + " - " + score.score;
-  
-      // display on page
-      var olEl = document.getElementById("highscores");
-      olEl.appendChild(liTag);
-    });
-  }
-  
-  function clearHighscores() {
-    window.localStorage.removeItem("highscores");
-    window.location.reload();
-  }
-  
-  document.getElementById("clear").onclick = clearHighscores;
-  
-  // run function when page loads
-  printHighscores();
